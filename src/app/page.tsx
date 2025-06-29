1b88db3ddfa3ae4f24c444ca44714dcb5e87a98b
Stefano S. Osuna <stefano.osuna@gmail.com>
2025-06-29 23:05:54 +0000

    now change that image for this one https://images.pexels.com/photos/1293

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default function LoginPage() {
     <div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2">
       <div className="relative hidden lg:block">
         <Image
-          src="https://images.unsplash.com/photo-1542435503-956c469947f6?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src="https://images.pexels.com/photos/12935064/pexels-photo-12935064.jpeg"
           alt="Abstract background"
           fill
           priority

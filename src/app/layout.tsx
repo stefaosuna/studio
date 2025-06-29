5af60682b7c6a43f0b17c802e68ecdb1e71dff50
Stefano S. Osuna <stefano.osuna@gmail.com>
2025-06-29 22:15:38 +0000

    still in lightmode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
       <body className="font-body antialiased">
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          forcedTheme="dark"
           disableTransitionOnChange
         >
           {children}
